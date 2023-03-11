1cd6ac049f213c3eb63bad1dec8b15ea272c6200
MAHMOUD-X-EMAD <mahmoudemadmahmoud1213@gmail.com>
2023-03-11 20:03:47 +0200

    Last 3/11 8:00

---
+++ Controllers/UserDataController.cs
@@ -0,0 +1,61 @@
+﻿using Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+namespace Final.Controllers
+{
+    public class UserDataController : Controller
+    {
+        Context context= new Context();
+        [HttpGet]
+        public IActionResult PersonalInfo()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CoverLetter()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Skill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Skill(Skill Newskill,string submit)
+        {
+            //if (submit == "Add")
+            //{
+            //    context.Skills.Add(Newskill);
+            //    context.SaveChanges();
+            //    ModelState.Clear();
+            //    return View();
+            //}
+            //else if (submit == "Next")
+            //{
+            //    return RedirectToAction("Experience");
+            //}
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Experience()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Project()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Link()
+        {
+            return View();
+        }
+    }
+}
